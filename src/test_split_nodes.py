f477f283f5fdc69d97a72ec24dc153c0601d519d
woolwood <35411775+woolwood@users.noreply.github.com>
2024-10-30 19:56:05 +0100

    Slight refactor: Grouping split_nodes_delimiter() in broader file split_nodes which will contain more functions.

--- src/test_split_nodes.py
+++ src/test_split_nodes.py
@@ -1,6 +1,6 @@
 import unittest
 from textnode import TextNode, TextType
-from split_nodes_delimiter import split_nodes_delimiter
+from split_nodes import split_nodes_delimiter
 
 
 class TestSplitNodesDelimiter(unittest.TestCase):
