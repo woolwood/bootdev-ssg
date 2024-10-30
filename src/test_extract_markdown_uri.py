27d918fe4879b7cc66ed9e029e09a9312ba958c1
woolwood <35411775+woolwood@users.noreply.github.com>
2024-10-30 19:45:38 +0100

    extract_markdown_links() now ignores images

--- src/test_extract_markdown_uri.py
+++ src/test_extract_markdown_uri.py
@@ -2,15 +2,32 @@ import unittest
 import re
 from extract_markdown_uri import extract_markdown_images, extract_markdown_links
 
+
 class TestExtractMarkdownUri(unittest.TestCase):
 
     def test_image_extraction(self):
 
-        text = "Two image uri's formatted in markdown: 1) ![Jolly good!](jollygood.jpg). 2) ![A cat looks at a mouse.](https://img.freepik.com/premium-photo/cat-looking-mouse-table_865967-64208.jpg)"
+        # Two image uri's formatted in markdown.abs
+        text = "1) ![Jolly good!](jollygood.jpg). 2) ![A cat looks at a mouse.](https://img.freepik.com/premium-photo/cat-looking-mouse-table_865967-64208.jpg)"
         test_result = extract_markdown_images(text)
         expected_result = [
             ("Jolly good!", "jollygood.jpg"),
-            ("A cat looks at a mouse.", "https://img.freepik.com/premium-photo/cat-looking-mouse-table_865967-64208.jpg")
+            (
+                "A cat looks at a mouse.",
+                "https://img.freepik.com/premium-photo/cat-looking-mouse-table_865967-64208.jpg",
+            ),
+        ]
+        self.assertEqual(test_result, expected_result)
+
+        # One URI with empty alt text.
+        text = "1) ![](jollygood.jpg). 2) ![A cat looks at a mouse.](https://img.freepik.com/premium-photo/cat-looking-mouse-table_865967-64208.jpg)"
+        test_result = extract_markdown_images(text)
+        expected_result = [
+            ("", "jollygood.jpg"),
+            (
+                "A cat looks at a mouse.",
+                "https://img.freepik.com/premium-photo/cat-looking-mouse-table_865967-64208.jpg",
+            ),
         ]
         self.assertEqual(test_result, expected_result)
 
@@ -22,8 +39,17 @@ class TestExtractMarkdownUri(unittest.TestCase):
             ("Google", "https://google.com"),
             ("DuckDuckGo", "https://duckduckgo.com"),
             ("Yandex", "https://yandex.com"),
-            ]
+        ]
         self.assertEqual(test_result, expected_result)
+
+    def test_ignore_img_link_extraction(self):
+
+        text = "Image URI to ignore: ![A cat looks at a mouse.](https://img.freepik.com/premium-photo/cat-looking-mouse-table_865967-64208.jpg) and a link to capture: [Google](https://google.com)"
+        test_result = extract_markdown_links(text)
+        expected_result = [("Google", "https://google.com")]
+
+        self.assertEqual(test_result, expected_result)
+
 
 if __name__ == "__main__":
     unittest.main()
